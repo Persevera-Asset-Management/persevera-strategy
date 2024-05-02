d0a31cea5dd410c6bc491cdc1683cae7e1412245
thales-carmo <thales.carmo@persevera.com.br>
2024-05-02 15:30:16 -0300

    Inclusão de melhorias @202405021530

--- pages/others.py
+++ pages/others.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 import streamlit as st
-import plotly.graph_objects as go
 import plotly.express as px
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'data')
