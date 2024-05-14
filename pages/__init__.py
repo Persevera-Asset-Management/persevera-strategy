a2afe74c5f69f8196b18f41584a72ecc4d7719be
thales-carmo <thales.carmo@persevera.com.br>
2024-05-14 11:01:38 -0300

    Inclusão de melhorias @202405141101

--- pages/__init__.py
+++ pages/__init__.py
@@ -2,6 +2,5 @@ from pages.home import show_home
 from pages.chartbook import show_chartbook
 from pages.fund_analysis import show_fund_analysis
 from pages.trinity import show_trinity
-from pages.nemesis import show_nemesis
 from pages.quant_screening import show_quant_screening
 from pages.tools import show_tools
