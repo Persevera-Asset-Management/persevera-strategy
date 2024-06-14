bef8388667389535354a17443c821ad5c4e45a10
thales-carmo <thales.carmo@persevera.com.br>
2024-06-14 11:48:14 -0300

    Inclusão de melhorias @202406141148

--- pages/home.py
+++ pages/home.py
@@ -6,5 +6,6 @@ def show_home():
     st.header("Persevera Asset Management")
 
     conn = st.connection('s3', type=FilesConnection)
-    df = conn.read("s3://persevera/multi_factor_screening.parquet", input_format='parquet')
+    df = conn.read("s3://persevera/factor_zoo.parquet", input_format='parquet')
+    df = df.query("code == 'VALE3'").filter(['price_close', 'ebitda_yield_fwd'])
     st.dataframe(df)
