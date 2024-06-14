f656758105093acaa3d9740ab76735576897b944
thales-carmo <thales.carmo@persevera.com.br>
2024-06-14 12:09:25 -0300

    Inclusão de melhorias @202406141209

--- pages/home.py
+++ pages/home.py
@@ -5,7 +5,7 @@ from st_files_connection import FilesConnection
 def show_home():
     st.header("Persevera Asset Management")
 
-    conn = st.connection('s3', type=FilesConnection)
-    df = conn.read("s3://persevera/factor_zoo.parquet", input_format='parquet')
-    df = df.query('code == "VALE3"').filter(['price_close', 'dividend_per_share'])
-    st.dataframe(df)
+    # conn = st.connection('s3', type=FilesConnection)
+    # df = conn.read("s3://persevera/factor_zoo.parquet", input_format='parquet')
+    # df = df.query('code == "VALE3"').filter(['price_close', 'dividend_per_share'])
+    # st.dataframe(df)
