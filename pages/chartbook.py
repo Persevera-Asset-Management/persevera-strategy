5acb9aabfde8edbf61adc7f118840384768a4b66
thales-carmo <thales.carmo@persevera.com.br>
2024-06-27 10:02:07 -0300

    Inclusão de melhorias @202406270956

--- pages/chartbook.py
+++ pages/chartbook.py
@@ -11,7 +11,7 @@ from st_files_connection import FilesConnection
 import utils
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'data')
-fs = utils.get_fs_connection("consolidado-indicators.parquet")
+# fs = utils.get_fs_connection("consolidado-indicators.parquet")
 
 
 def get_data(fs, fields: list):
